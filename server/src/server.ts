d041f73684ce61a5cf729cd3d105fa648ec10e91
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-02-18 10:19:28 -0300

    feat: implement schema validation with Fastify

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,37 @@
-import { fastifyCors } from './../node_modules/@fastify/cors/types/index.d';
+import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod";
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.register(fastifyCors)
-app.get("/hello", () => {
-   return "Hello world"
-})
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
-app.listen({port: 3333}).then(() => {
-   console.log("HTTP server running!")
-})
+app.register(fastifyCors);
+app.post(
+  "/subscription",
+  {
+    schema: {
+      body: z.object({
+        name: z.string(),
+        email: z.string().email(),
+      }),
+    },
+  },
+  (request, replay) => {
+    const { name, email } = request.body;
+
+    return replay.status(201).send({
+      name,
+      email,
+    });
+  }
+),
+  app.listen({ port: 3333 }).then(() => {
+    console.log("HTTP server running!");
+  });
