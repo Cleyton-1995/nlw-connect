9881d8167159acb2d6a94a246caf0d426754888b
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-02-18 09:56:40 -0300

    chore: install and configure fastify-cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
+import { fastifyCors } from './../node_modules/@fastify/cors/types/index.d';
 import fastify from "fastify";
 
 const app = fastify()
 
+app.register(fastifyCors)
 app.get("/hello", () => {
    return "Hello world"
 })
