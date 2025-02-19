b0c08ed6defcb0e4e6a7343ef9661d22b7f3a4e1
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-02-19 16:41:06 -0300

    chore: minor adjustments and improvements

--- server/src/routes/subscrible-to-event-route.ts
+++ server/src/routes/subscrible-to-event-route.ts
@@ -11,7 +11,7 @@ export const SubscribleToEventRoute: FastifyPluginAsyncZod = async app => {
         tags: ['Subscription'],
         body: z.object({
           name: z.string(),
-          email: z.string(),
+          email: z.string().email(),
           referrer: z.string().nullish(),
         }),
         response: {
