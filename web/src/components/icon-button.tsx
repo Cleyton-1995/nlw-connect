2a98204cf0db4dd68606c2b265e3ef31ecede547
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-02-20 15:41:57 -0300

    feat: extend button properties

--- web/src/components/icon-button.tsx
+++ web/src/components/icon-button.tsx
@@ -1,13 +1,12 @@
-import type { ReactNode } from "react";
+import type { ComponentProps } from "react";
 
-interface ButtonProps {
-  children: ReactNode;
-}
+interface ButtonProps extends ComponentProps<"button"> {}
 
 export function IconButton(props: ButtonProps) {
   return (
-    <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900">
-      {props.children}
-    </button>
+    <button
+      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      {...props}
+    />
   );
 }
